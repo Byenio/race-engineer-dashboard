545851ac2733f8b268c9e3131e91509c157df6d8
byenio <marcel.bienko@gmail.com>
2024-11-15 17:44:56 +0100

    ♻️ Refactor Card component

--- app/(components)/Card.tsx
+++ app/(components)/Card.tsx
@@ -1,36 +1,23 @@
 import { ReactNode } from "react";
 
-type Colors = "red" | "green" | "blue" | "white";
-
-type PresetColors = {
-  [key in Colors]: string;
-};
-
 export default function Card({
-  className,
-  cardName,
-  noBorder,
-  accentColor = "white",
+  className = "",
+  cardName = "",
+  noBorder = false,
+  accentColor = "rgb(255, 255, 255)",
   children,
 }: {
   className?: string;
   cardName?: string;
   noBorder?: boolean;
-  accentColor?: Colors;
+  accentColor?: string;
   children: ReactNode;
 }) {
-  const border = noBorder ? "border-0 p-0" : "border-2 p-2";
-
-  const presetColors: PresetColors = {
-    red: "border-[#ff000060]",
-    green: "border-[#00ff0060]",
-    blue: "border-[#0000ff60]",
-    white: "border-[#ffffff60]",
-  };
+  const borderClass = noBorder ? "border-0 p-0" : "border-2 p-2";
 
   return (
     <div
-      className={`${border} ${presetColors[accentColor]} rounded-lg relative ${className}`}
+      className={`${borderClass} border-[${accentColor}] rounded-lg relative ${className}`}
     >
       <CardName cardName={cardName} />
       {children}
@@ -39,11 +26,11 @@ export default function Card({
 }
 
 function CardName({ cardName }: { cardName: string | undefined }) {
-  if (cardName)
-    return (
-      <span className="absolute top-[-13px] left-2 w-auto text-center bg-black px-2 text-amber-400">
-        {cardName.toUpperCase()}
-      </span>
-    );
-  return;
+  if (!cardName) return null;
+
+  return (
+    <span className="absolute top-[-13px] left-2 w-auto text-center bg-black px-2 text-amber-400">
+      {cardName.toUpperCase()}
+    </span>
+  );
 }
